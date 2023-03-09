1e4d5b49738589fc2db1d532c6f21c3c756536ea
syniti-marcio <marcio.casagrande@syniti.com>
2023-03-09 19:32:20 -0300

    STEP 06: Modules

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,12 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
         onShowHello : function () {
             // Show a native or vanilla JS alert
             alert("Hello there!");
+            // Show a message toast loading a default SAPUI5 module/component
+            MessageToast.show("Hello there!");
         }
     });
 })
