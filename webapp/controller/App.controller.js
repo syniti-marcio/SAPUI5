2b0b121eae578a6daaf2485dac46832683349075
syniti-marcio <marcio.casagrande@syniti.com>
2023-03-10 21:31:48 -0300

    STEP 15: Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,9 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.App", {
-        onShowHello: function () {
-            // read message from i18n model
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMessage = oBundle.getText("helloMessage", [sRecipient]);
-
-            // Show a native or vanilla JS alert
-            alert(sMessage);
-
-            // Show a message toast loading a default SAPUI5 module/component
-            MessageToast.show(sMessage);
-        }
+        
     });
-})
+});
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,21 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+
+    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+        onShowHello: function () {
+            // read message from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMessage = oBundle.getText("helloMessage", [sRecipient]);
+
+            // Show a native or vanilla JS alert
+            alert(sMessage);
+
+            // Show a message toast loading a default SAPUI5 module/component
+            MessageToast.show(sMessage);
+        }
+    });
+});
