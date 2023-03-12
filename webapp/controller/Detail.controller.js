7725b7bf70352075de6d2e4a9d184bb6e2f1d703
syniti-marcio <marcio.casagrande@syniti.com>
2023-03-12 11:29:30 -0300

    STEP 34: Custom Controls

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/core/UIComponent",
-    "sap/ui/core/routing/History"
-], function (Controller, UIComponent, History) {
+    "sap/ui/core/routing/History",
+    "sap/m/MessageToast"
+], function (Controller, UIComponent, History, MessageToast) {
     "use strict";
 
     return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
@@ -12,6 +13,7 @@ sap.ui.define([
         },
 
         _onObjectMatched: function (oEvent) {
+            this.byId("rating").reset();
             this.getView().bindElement({
                 path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
                 model: "invoice"
@@ -28,6 +30,13 @@ sap.ui.define([
                 var oRouter = UIComponent.getRouterFor(this);
                 oRouter.navTo("overview", {}, true);
             }
-        }
+        },
+
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+		}
     });
 });
